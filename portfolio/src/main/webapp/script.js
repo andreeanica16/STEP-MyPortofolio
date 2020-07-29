949739ea3b68bf66ff718527336c7b4771211c20
Andreea Nica <andre.nic10@yahoo.com>
2020-07-29 09:44:03 +0000

    Format the map style and add a home marker

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -320,5 +320,169 @@
 function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+      {
+          center: {lat: 47.889019, lng: 3.7831301},
+          zoom: 5,
+          styles:[
+              {
+                  "elementType": "geometry",
+                  "stylers": [{"color": "#1d2c4d"}]
+              },
+              {
+                  "elementType": "labels.text.fill",
+                  "stylers": [{"color": "#8ec3b9"}]
+              },
+              {
+                  "elementType": "labels.text.stroke",
+                  "stylers": [{"color": "#1a3646"}]
+              },
+              {
+                  "featureType": "administrative.country",
+                  "elementType": "geometry.stroke",
+                  "stylers": [{"color": "#7faa09"}, 
+                              {"visibility": "on"},
+                              {"weight": 1},
+                              ]
+              },
+              {
+                  "featureType": "administrative.country",
+                  "elementType": "labels.text.stroke",
+                  "stylers": [{"visibility": "on"}]
+              },
+              {
+                  "featureType": "administrative.land_parcel",
+                  "elementType": "labels.text.fill",
+                  "stylers": [{"color": "#64779e" }]
+              },
+              {
+                  "featureType": "administrative.province",
+                  "elementType": "geometry.stroke",
+                  "stylers": [{"color": "#4b6878"}]
+              },
+              {
+                  "featureType": "landscape.man_made",
+                  "elementType": "geometry.stroke",
+                  "stylers": [{"color": "#334e87"}]
+              },
+              {
+                  "featureType": "landscape.natural",
+                  "elementType": "geometry",
+                  "stylers": [{"color": "#023e58"}]
+              },
+              {
+                  "featureType": "poi",
+                  "elementType": "geometry",
+                  "stylers": [{"color": "#283d6a"}]
+              },
+              {
+                  "featureType": "poi",
+                  "elementType": "labels.text.fill",
+                  "stylers": [{"color": "#6f9ba5"}]
+              },
+              {
+                  "featureType": "poi",
+                  "elementType": "labels.text.stroke",
+                  "stylers": [{"color": "#1d2c4d"}]
+              },
+              {
+                  "featureType": "poi.park",
+                  "elementType": "geometry.fill",
+                  "stylers": [{"color": "#023e58"}]
+              },
+              {
+                  "featureType": "poi.park",
+                  "elementType": "labels.text.fill",
+                  "stylers": [{"color": "#3C7680"}]
+              },
+              {
+                  "featureType": "road",
+                  "elementType": "geometry",
+                  "stylers": [{"color": "#304a7d"}]
+              },
+              {
+                  "featureType": "road",
+                  "elementType": "labels.text.fill",
+                  "stylers": [{"color": "#98a5be"}]
+              },
+              {
+                  "featureType": "road",
+                  "elementType": "labels.text.stroke",
+                  "stylers": [{"color": "#1d2c4d"}]
+              },
+              {
+                  "featureType": "road.highway",
+                  "elementType": "geometry",
+                  "stylers": [{"color": "#2c6675"}]
+              },
+              {
+                  "featureType": "road.highway",
+                  "elementType": "geometry.stroke",
+                  "stylers": [ {"color": "#255763"}]
+              },
+              {
+                  "featureType": "road.highway",
+                  "elementType": "labels.text.fill",
+                  "stylers": [{"color": "#b0d5ce"}]
+              },
+              {
+                  "featureType": "road.highway",
+                  "elementType": "labels.text.stroke",
+                  "stylers": [{"color": "#023e58"}]
+              },
+              {
+                  "featureType": "transit",
+                  "elementType": "labels.text.fill",
+                  "stylers": [{"color": "#98a5be"}]
+              },
+              {
+                  "featureType": "transit",
+                  "elementType": "labels.text.stroke",
+                  "stylers": [{"color": "#1d2c4d"}]
+              },
+              {
+                  "featureType": "transit.line",
+                  "elementType": "geometry.fill",
+                  "stylers": [{"color": "#283d6a"}]
+              },
+              {
+                  "featureType": "transit.station",
+                  "elementType": "geometry",
+                  "stylers": [{"color": "#3a4762"}]
+              },
+              {
+                  "featureType": "water",
+                  "elementType": "geometry",
+                  "stylers": [{"color": "#0e1626"}]
+              },
+              {
+                  "featureType": "water",
+                  "elementType": "labels.text.fill",
+                  "stylers": [{"color": "#4e6d70"}]
+              }
+        ]            
+    });
+  map.setMapTypeId('terrain');
+
+  const romania = {lat: 45.9432, lng: 24.9668};
+  const marker = new google.maps.Marker({
+    position: romania,
+    icon: {
+            url:'http://maps.google.com/mapfiles/kml/shapes/homegardenbusiness.png',
+            size: new google.maps.Size(32, 32),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 32),
+            scaledSize: new google.maps.Size(32, 32),
+          },
+    map: map,
+    title: 'My Home Country',
+    animation: google.maps.Animation.DROP,
+  });
+
+  marker.addListener('click', function () {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  });
 }
